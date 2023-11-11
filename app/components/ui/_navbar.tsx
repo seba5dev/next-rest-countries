2752d4b8d215cf36d2be1be873b901abf8711a7e
Sebastian Martinez <sebasmartinez05@outlook.com>
2023-11-10 20:46:34 -0500

    initial commit

---
+++ app/components/ui/_navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center w-full shadow-lg my-5 px-5">
+      <a className="">Where in the world?</a>
+      <button>Dark Mode</button>
+    </nav>
+  );
+}
