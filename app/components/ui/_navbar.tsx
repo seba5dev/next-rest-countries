f30053ce0397a38bcc6169ad9cee8d7dba945fad
Sebastian Martinez <sebasmartinez05@outlook.com>
2023-11-12 21:31:51 -0500

    fix on colors in navbar

--- app/components/ui/_navbar.tsx
+++ app/components/ui/_navbar.tsx
@@ -3,8 +3,10 @@ import ThemeSwitch from "./themes/_themeSwitch";
 
 export default function Navbar() {
   return (
-    <nav className="flex w-full items-center justify-between px-10 py-5 shadow">
-      <Link href="/" className="font-bold cursor-pointer">Where in the world?</Link>
+    <nav className="bg-white flex w-full items-center justify-between px-5 py-5 shadow dark:bg-darkBlue md:px-20">
+      <Link href="/" className="md:text-xl cursor-pointer font-bold">
+        Where in the world?
+      </Link>
       <ThemeSwitch />
     </nav>
   );
