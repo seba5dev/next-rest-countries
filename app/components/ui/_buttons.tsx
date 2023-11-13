cde05833654fa5c28dd96bd0327fc3876ce0d1b1
Sebastian Martinez <sebasmartinez05@outlook.com>
2023-11-12 20:32:11 -0500

    create button component to normalize styles

---
+++ app/components/ui/_buttons.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+function Button({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href: string;
+}) {
+  if (href)
+    return (
+      <Link
+        className="flex flex-row items-center justify-center gap-x-2 bg-white px-4 py-1 text-veryDarkBlue2 shadow-md dark:bg-darkBlue dark:text-white"
+        href={href}
+      >
+        {children}
+      </Link>
+    );
+
+  return (
+    <button className="flex flex-row items-center justify-center gap-x-2 bg-white px-4 py-1 text-veryDarkBlue2 shadow-md dark:bg-darkBlue dark:text-white">
+      {children}
+    </button>
+  );
+}
+
+export default Button;
