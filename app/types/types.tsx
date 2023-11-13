a536a32b3714da8c0431d57c6e3e5c1ffadeaab6
Sebastian Martinez <sebasmartinez05@outlook.com>
2023-11-12 21:12:41 -0500

    update types for details page

--- app/types/types.tsx
+++ app/types/types.tsx
@@ -1,22 +1,35 @@
-export type Theme = 'dark' | 'light';
-
-export interface GeneralContextType {
-  theme: Theme;
-  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
-  toggleTheme: () => void;
-  // ... otros tipos generales
-}
+export type Theme = "dark" | "light";
 
 export interface Country {
   name: {
     common: string;
+    nativeName?: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
   };
+  tld?: string[];
+  cca2: string;
+  currencies?: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  capital: string;
+  region: string;
+  subregion?: string;
+  languages?: {
+    [key: string]: string;
+  };
+  borders?: string[];
+  population: number;
   flags: {
     svg: string;
+    alt: string;
   };
-  population: number;
-  region: string;
-  capital: string;
 }
 
 export interface Countries {
